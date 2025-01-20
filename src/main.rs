0200f7dbe15291efc0f4b5d89112cb6689a80574
karlosvas <carlosraul.vasquez.sanchez@gregoriofer.com>
2025-01-20 12:48:54 +0100

    Programming a guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("¡Hola, mundo!");
+    println!("Gess the number!");
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("Your guessed: {}", guess);
+    // println!("Your guessed: {guess}");
 }
